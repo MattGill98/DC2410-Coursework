fee29a6cbe1f047064d4f7b54650765d0be86a65
Matt Gill <matthew.gill@live.co.uk>
2018-07-31 15:58:06 +0100

    Added tick to sort dropdown.

--- client/src/components/SortDropdown.jsx
+++ client/src/components/SortDropdown.jsx
@@ -8,23 +8,35 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-    sortByPopularity: () => {dispatch(sortEvents('me'))},
-    sortByName: () => {dispatch(sortEvents('subscribed'))},
-    sortByCategory: () => {dispatch(sortEvents('sport'))},
-    sortByVenue: () => {dispatch(sortEvents('culture'))},
-    sortByOrganiser: () => {dispatch(sortEvents('others'))},
+    sortByPopularity: () => {dispatch(sortEvents('popularity'))},
+    sortByName: () => {dispatch(sortEvents('name'))},
+    sortByDate: () => {dispatch(sortEvents('date'))},
+    sortByCategory: () => {dispatch(sortEvents('category'))},
+    sortByVenue: () => {dispatch(sortEvents('venue'))},
+    sortByOrganiser: () => {dispatch(sortEvents('organiser'))},
     reverseSortOrder: () => {dispatch(reverseSortOrder())}
 });
 
-const SortDropdown = ({sortValue, sortByPopularity, sortByName, sortByCategory, sortByVenue, sortByOrganiser}) => (
+const TickedDropdownItem = ({name, sortValue, event}) => {
+    var ticked = null;
+    if (sortValue === name.toLowerCase()) {
+        ticked = 'dropdown-item-checked';
+    }
+    return (
+        <DropdownItem className={ticked} onClick={e => event()}>{name}</DropdownItem>
+    );
+};
+
+const SortDropdown = ({sortValue, sortByPopularity, sortByName, sortByDate, sortByCategory, sortByVenue, sortByOrganiser}) => (
     <UncontrolledDropdown>
         <DropdownToggle caret>Sort by</DropdownToggle>
         <DropdownMenu>
-            <DropdownItem onClick={e => sortByPopularity()}>Popularity</DropdownItem>
-            <DropdownItem onClick={e => sortByName()}>Name</DropdownItem>
-            <DropdownItem onClick={e => sortByCategory()}>Category</DropdownItem>
-            <DropdownItem onClick={e => sortByVenue()}>Venue</DropdownItem>
-            <DropdownItem onClick={e => sortByOrganiser()}>Organiser</DropdownItem>
+            <TickedDropdownItem name="Popularity" sortValue={sortValue} event={sortByPopularity} />
+            <TickedDropdownItem name="Name" sortValue={sortValue} event={sortByName} />
+            <TickedDropdownItem name="Date" sortValue={sortValue} event={sortByDate} />
+            <TickedDropdownItem name="Category" sortValue={sortValue} event={sortByCategory} />
+            <TickedDropdownItem name="Venue" sortValue={sortValue} event={sortByVenue} />
+            <TickedDropdownItem name="Organiser" sortValue={sortValue} event={sortByOrganiser} />
         </DropdownMenu>
     </UncontrolledDropdown>
 );
